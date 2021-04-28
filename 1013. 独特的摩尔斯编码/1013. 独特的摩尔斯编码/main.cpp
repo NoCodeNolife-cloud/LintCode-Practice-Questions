805889ffc15b355cfd2512dddad5d367761f479e
蔡伟 <1607575130@qq.com>
2021-04-28 15:54:16 +0800

    update

---
+++ 1013. 独特的摩尔斯编码/1013. 独特的摩尔斯编码/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param words: the given list of words
+	 * @return: the number of different transformations among all words we have
+	 */
+	int uniqueMorseRepresentations ( vector<string>& words ) {
+		string cnt [] = { ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.." };
+		set<string> coll;
+		for ( string item : words ) {
+			string tmp;
+			for ( int i = 0; i < item.size (); ++i ) {
+				tmp += cnt [ item [ i ] - 'a' ];
+			}
+			coll.insert ( tmp );
+		}
+		return coll.size ();
+	}
+};
+
+int main () {
+	Solution solution;
+	vector<string> words = { "gin", "zen", "gig", "msg" };
+	cout << solution.uniqueMorseRepresentations ( words ) << std::endl;
+	system ( "PAUSE" );
+}
